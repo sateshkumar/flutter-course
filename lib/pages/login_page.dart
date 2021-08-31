e2f8c2761c4110832196b37f0e2a59f63c08f550
satesh <sateshkumar2515@gmail.com>
2021-08-31 20:49:33 +0500

    user changes

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset('assets/images/login.png'),
+          Image.asset('assets/images/login.png'),
+          Image.asset('assets/images/login.png'),
+        ],
+      ),
+    );
+  }
+}
