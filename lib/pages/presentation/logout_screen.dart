a407a045f4e37d0d79344032b9920df68d1ebcf8
satesh <sateshkumar2515@gmail.com>
2021-09-01 15:39:02 +0500

    added logout screen

---
+++ lib/pages/presentation/logout_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LogoutScreen extends StatefulWidget {
+  LogoutScreen({Key key}) : super(key: key);
+
+  @override
+  _LogoutScreenState createState() => _LogoutScreenState();
+}
+
+class _LogoutScreenState extends State<LogoutScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
